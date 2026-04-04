c18f26ecc3f2a89fca43514289b229c72cf9211b
ZL Tech <zltech64@gmail.com>
2026-04-03 23:35:26 -0300

    feat: adicionando login google

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -32,3 +32,50 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         msg.style.color = 'red';
     });
 });
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "SEU_ID_CLIENTE_AQUI.apps.googleusercontent.com",
+        callback: handleLoginResponse
+    });
+
+    google.accounts.id.renderButton(
+        document.getElementById("google-btn-container"),
+        { 
+            theme: "filled_black", // Preto para não ser azul
+            size: "medium",       // "medium" é menor que o "large" que você estava usando
+            width: 250,           // Ajuste este número para diminuir a largura
+            shape: "rectangular",
+            locale: "pt-BR"
+        }
+    );
+}
+
+
+/*
+function handleLoginResponse(response) {
+    // 1. O Google autenticou o usuário e te deu um Token (JWT)
+    const token = response.credential;
+
+    // 2. Enviar para o seu backend para validar e iniciar a SESSION
+    fetch('http://localhost/negocio_area/public/api/index.php/api/login_social', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: token })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.status === 'success') {
+            // Login feito! Redireciona para a dashboard
+            window.location.href = 'dashboard.php';
+        } else {
+            // Caso o e-mail não esteja cadastrado ainda
+            msg.innerText = "Conta não encontrada. Por favor, cadastre-se primeiro.";
+            msg.className = "msg erro";
+        }
+    })
+    .catch(err => console.error("Erro no login social:", err));
+}
+    */
